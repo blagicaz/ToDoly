def6ae7eaa4a7537a271cf4e28caad59a9156676
blagicaz <blagica.z@gmail.com>
2018-10-10 23:15:03 +0200

    add more tests

--- test/com/todoly/TaskListTest.java
+++ test/com/todoly/TaskListTest.java
@@ -1,7 +1,9 @@
 package com.todoly;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -10,32 +12,33 @@ import static org.junit.Assert.assertTrue;
 
 public class TaskListTest {
 
-    private TaskList createTaskList() {
+    private TaskList emptyTaskList;
+    private TaskList taskList;
+
+    @Before
+    public void setUp() {
+        emptyTaskList = new TaskList();
+        taskList = new TaskList();
+
         Task t1 = new Task("test task 1", new Date(), "project 1" );
         Task t2 = new Task("test task 2", new Date(), "project 2" );
         Task t3 = new Task("test task 3", new Date(), "project 3" );
         Task t4 = new Task("test task 4", new Date(), "project 4" );
-        TaskList taskList = new TaskList();
+
         taskList.addTask(t1);
         taskList.addTask(t2);
         taskList.addTask(t3);
         taskList.addTask(t4);
-        return taskList;
-    }
-
-    @Test
-    public void testValidIndexOnEmptyList() {
-        TaskList taskList = new TaskList();
-
-        assertFalse(taskList.validIndex(0));
-        assertFalse(taskList.validIndex(3));
-        assertFalse(taskList.validIndex(19));
     }
 
     @Test
     public void testValidIndex() {
-        TaskList taskList = createTaskList();
+        // Test on a empty taskList
+        assertFalse(emptyTaskList.validIndex(0));
+        assertFalse(emptyTaskList.validIndex(3));
+        assertFalse(emptyTaskList.validIndex(19));
 
+        // Tests with some tasks
         assertEquals(true, taskList.validIndex(0));
         assertTrue(taskList.validIndex(0));
         assertTrue(taskList.validIndex(3));
@@ -44,44 +47,58 @@ public class TaskListTest {
     }
 
     @Test
-    public void testNumberDoneOnEmptyList() {
-        TaskList taskList = new TaskList();
-
-        int num = taskList.numberDone();
-        assertEquals(0, num);
-    }
-
-    @Test
     public void testNumberDone() {
-        TaskList taskList = createTaskList();
+        // Test on a empty taskList
+        int num = emptyTaskList.numberDone();
+        assertEquals(0, num);
 
+        // Tests with completed tasks
         assertEquals(0, taskList.numberDone());
-
         taskList.getTasks().get(0).setComplete(true);
         assertEquals(1, taskList.numberDone());
-
         taskList.getTasks().get(1).setComplete(true);
         assertEquals(2, taskList.numberDone());
     }
 
     @Test
-    public void testNumberNotDoneOnEmptyList() {
-        TaskList taskList = new TaskList();
-
-        int num = taskList.numberNotDone();
+    public void testNumberNotDone() {
+        // Test on a empty taskList
+        int num = emptyTaskList.numberNotDone();
         assertEquals(0, num);
+
+        // Tests with not completed tasks
+        assertEquals(4, taskList.numberNotDone());
+        taskList.getTasks().get(0).setComplete(true);
+        assertEquals(3, taskList.numberNotDone());
+        taskList.getTasks().get(1).setComplete(true);
+        assertEquals(2, taskList.numberNotDone());
     }
 
     @Test
-    public void testNumberNotDone() {
-        TaskList taskList = createTaskList();
+    public void testNotDoneTasks() {
+        // Test on a empty taskList
+        ArrayList<Task> result = emptyTaskList.notDoneTasks();
+        assertTrue(result.isEmpty());
 
-        assertEquals(4, taskList.numberNotDone());
+        // Tests with not completed tasks
+        result = taskList.notDoneTasks();
+        assertEquals(4, result.size());
 
+        // Test if the the result list has the correct size
         taskList.getTasks().get(0).setComplete(true);
-        assertEquals(3, taskList.numberNotDone());
-
         taskList.getTasks().get(1).setComplete(true);
-        assertEquals(2, taskList.numberNotDone());
+        result = taskList.notDoneTasks();
+        assertEquals(2, result.size());
+
+        // Test if all the tasks in the result are not complete
+        assertFalse(result.get(0).getComplete());
+        assertFalse(result.get(1).getComplete());
+    }
+
+    @Test
+    public void testFilterByProject() {
+        ArrayList<Task> result = taskList.filterByProject("project 1");
+        assertEquals(1, result.size());
+        assertEquals("project 1", result.get(0).getProject());
     }
 }
